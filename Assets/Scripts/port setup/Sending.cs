2202a1ba42d0f8dbfbe9afccb6105f5abf874319
Austin Craigie <apcraigie@live.com>
2017-10-14 14:18:18 -0700

    added serial communication script

---
+++ Assets/Scripts/port setup/Sending.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.IO.Ports;
+
+public class Sending : MonoBehaviour {
+
+	public static SerialPort sp = new SerialPort("COM3", 9600);
+	public string message;
+
+	// Use this for initialization
+	void Start () {
+		OpenConnection();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OpenConnection()
+	{
+		if (sp != null) 
+		{
+			if (sp.IsOpen) 
+			{
+				sp.Close();
+				print("Closing port, because it was already open!");
+			}
+			else 
+			{
+				sp.Open();  // opens the connection
+				sp.ReadTimeout = 16;  // sets the timeout value before reporting error
+				print("Port Opened!");
+				//		message = "Port Opened!";
+			}
+		}
+		else 
+		{
+			if (sp.IsOpen)
+			{
+				print("Port is already open");
+			}
+			else 
+			{
+				print("Port == null");
+			}
+		}
+	}
+
+	void OnApplicationQuit() 
+	{
+		sp.Close();
+	}
+}
