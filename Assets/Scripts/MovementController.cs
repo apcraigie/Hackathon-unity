3c51c33affa6376e738876ac8ecbb4c83cad02d3
Logan Williamson <williamson.logan@gmail.com>
2017-10-14 19:20:27 -0700

    Controls set up

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1
+        if (Controller.GetAxis() != Vector2.zero)
+        {
+            //Up Moves Robot Forwards
+
+            //Down Move Robot Backwards
+
+            //Right Rotate Robot to the Right
+
+            //Left Rotate Robot to the Left
+            Debug.Log(gameObject.name + Controller.GetAxis());
+        }
+
+        // 2
+        if (Controller.GetHairTriggerDown())
+        {
+            Debug.Log(gameObject.name + " Trigger Press");
+            //Lift Left Arm
+        }
+
+        // 3
+        if (Controller.GetHairTriggerUp())
+        {
+            Debug.Log(gameObject.name + " Trigger Release");
+            //Release Left Arm
+        }
+
+
+
+        // 4
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log(gameObject.name + " Grip Press");
+            //Magnet ON
+        }
+
+        // 5
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log(gameObject.name + " Grip Release");
+            //Magnet OFF
+        }
+    }
+}
--- Assets/Scripts/WebcamStream.cs
+++ Assets/Scripts/WebcamStream.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class WebcamStream : MonoBehaviour {
 
     public RawImage rawimage;
+    public Camera VRHead;
+
+    Vector3 CamRot;
     void Start()
     {
         WebCamTexture webcamTexture = new WebCamTexture();
@@ -13,5 +16,9 @@ public class WebcamStream : MonoBehaviour {
         rawimage.material.mainTexture = webcamTexture;
         //webcamTexture.deviceName = "INSERT ARDUINO SOURCE HERE";
         webcamTexture.Play();
+
+        Transform vrrot;
+        vrrot = VRHead.transform;
+        CamRot = new Vector3(vrrot.rotation.x, vrrot.rotation.y, vrrot.rotation.z);
     }
 }
